1dd3e64d2ec5c4cb62b876ac60f9d85164105762
Jane11eR <janellericketts2007@gmail.com>
2024-12-08 23:51:31 -0800

    Update Event04.java

--- src/main/java/event/Event04.java
+++ src/main/java/event/Event04.java
@@ -2,17 +2,33 @@ package event;
 
 import main.GameManager;
 
+/**
+ * Manages scenes and actions for Event04.
+ */
 public class Event04 {
 
     GameManager gm;
 
+    /**
+     * Sets up Event04 with the game manager.
+     * 
+     * @param gm the main game manager
+     */
     public Event04(GameManager gm) {
 
         this.gm = gm;
     }
+
+    /**
+     * Shows the description of Denzel.
+     */
     public void lookDenzel() {
         gm.ui.messageText.setText("This is Denzel. (He listens to Valorant music)");
     }
+
+     /**
+     * Lets Denzel give the player a pistol if they don't already have one.
+     */
     public void talkDenzel() {
         if(gm.player.hasPistol == 0) {
             gm.player.hasPistol = 1;
@@ -26,6 +42,10 @@ public class Event04 {
             gm.playSE(gm.cannotSound);
         }
     }
+
+     /**
+     * Lets the player recover life with Denzel's help.
+     */
     public void restDenzel() {
         if(gm.player.playerLife != gm.player.playerMaxLife) {
             gm.ui.messageText.setText("(Denzel has been lonely without his girlfriend)\n*Denzel runs up to you unexpectedly and gives you a hug*\n(Your life has been recovered)");
@@ -39,4 +59,8 @@ public class Event04 {
             gm.playSE(gm.cannotSound);
         }
     }
+
+    // TODO: Check that the game manager (gm) is not null when the class is created.
+    // TODO: Create a helper method for repeated life recovery code.
+    // TODO: Make sure the pistol value (hasPistol) is either 0 or 1 only.
 }
