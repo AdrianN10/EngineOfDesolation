b32c11db039aed24815fd5267363f2634c71f3e1
Jane11eR <janellericketts2007@gmail.com>
2024-12-08 23:43:38 -0800

    Update Event02.java

--- src/main/java/event/Event02.java
+++ src/main/java/event/Event02.java
@@ -2,20 +2,41 @@ package event;
 
 import main.GameManager;
 
+/**
+* This class manages the scenes and actions for Event02
+*/
+
 public class Event02 {
 
     GameManager gm;
 
+    /**
+    * Sets up Event02 with the game manager.
+    *
+    * @param gm the main game manager
+    */
     public Event02(GameManager gm) {
 
         this.gm = gm;
     }
+
+    /**
+     * Checks the first spot on the Honda.
+     */
     public void search1Honda() {
         gm.ui.messageText.setText("Nothing sus here");
     }
+    
+    /**
+     * Checks the second spot on the Honda.
+     */
     public void search2Honda() {
         gm.ui.messageText.setText("OH MY GYAT MY ENGINE IS MISSING");
     }
+
+    /**
+     * Lets the player rest on the Honda and heal life.
+     */
     public void restHonda() {
         if(gm.player.playerLife != gm.player.playerMaxLife) {
             gm.ui.messageText.setText("*You rest on the hood of your car*\n(Your life has been recovered)");
@@ -29,4 +50,7 @@ public class Event02 {
 
         }
     }
+    // TODO: Make sure the game manager (gm) is not null in the constructor.
+    // TODO: Combine repeated actions like updating player status into a helper method.
+    // TODO: Check that player life values are not negative to avoid errors.
 }
