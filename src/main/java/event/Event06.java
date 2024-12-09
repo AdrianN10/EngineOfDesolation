f00916a56a0a13661f934ea72b20ae8a25cb4208
Jane11eR <janellericketts2007@gmail.com>
2024-12-09 00:07:27 -0800

    Update Event06.java

--- src/main/java/event/Event06.java
+++ src/main/java/event/Event06.java
@@ -3,27 +3,49 @@ package event;
 import javafx.scene.image.ImageView;
 import main.GameManager;
 
+/**
+ * Handles scenes and actions for Event06.
+ */
 public class Event06 {
     GameManager gm;
     private ImageView denzel;
 
+    /**
+     * Sets up Event06 with the game manager.
+     * 
+     * @param gm the main game manager
+     */
     public Event06(GameManager gm) {
 
         this.gm = gm;
     }
+
+    /**
+     * Resets Denzel's visibility.
+     */
     public void reset(){
         if (denzel != null) {
             denzel.setVisible(false);
         }
     }
-
+    
+    /**
+     * Shows Denzel.
+     */
     public void lookDenzel(){
         gm.ui.messageText.setText("Denzel shows up");
     }
+
+    /**
+     * Talks to Denzel.
+     */
     public void talkDenzel(){
         gm.ui.messageText.setText("I got worried for you so I ran after you");
-
     }
+
+    /**
+     * Plays Valorant music to cheer up Denzel.
+     */
     public void playDenzel(){
 
         if(gm.player.hasAk47 == 0) {
@@ -40,12 +62,23 @@ public class Event06 {
 
     }
 
+    /**
+     * Shows Brandon.
+     */
     public void lookBrandon() {
         gm.ui.messageText.setText("This is Brandon. He seems to be doing a fit check");
     }
+
+    /**
+     * Talks to Brandon.
+     */
     public void talkBrandon() {
         gm.ui.messageText.setText("Yo I wouldn't go in there if I were you! A monster is on the loose!");
     }
+
+    /**
+     * Lets the player rest and recover life with Brandon's help.
+     */
     public void restBrandon() {
         if(gm.player.playerLife != gm.player.playerMaxLife) {
             gm.ui.messageText.setText("You look injured... Come here\n*Brandon gives you a warm hug*\n(You gain life)");
@@ -70,4 +103,8 @@ public class Event06 {
         }
 
     }
+
+// TODO: Check if gm and Denzel are not null before using them.
+// TODO: Create a method to play sounds instead of repeating the code.
+// TODO: Make sure hasAk47 is valid before using it.
 }
